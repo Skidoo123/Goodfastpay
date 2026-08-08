7224c034ac706ae04728dd47031c124f6fbb49d4
Skidoo123 <157570758+Skidoo123@users.noreply.github.com>
2026-08-08 16:28:58 +0100

    feat(firebase): add modular firebase.js configuration module

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCGWeOGc0ANw8KKgtONqFzAdhWeQACTvWE",
+  authDomain: "good-fast-pay.firebaseapp.com",
+  projectId: "good-fast-pay",
+  storageBucket: "good-fast-pay.firebasestorage.app",
+  messagingSenderId: "1010521921961",
+  appId: "1:1010521921961:web:584ebd5170241bd2778cb8",
+  measurementId: "G-BHGFGEG7WE"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+export const db = getFirestore(app);
+export const auth = getAuth(app);
